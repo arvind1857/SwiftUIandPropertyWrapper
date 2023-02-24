90be6b99074e0c7fa3f3b4507be4e453fd6b67b2
arvind1857 <63805884+arvind1857@users.noreply.github.com>
2023-02-24 19:17:44 +0530

    Add files via upload

---
+++ EnviornmentObject/EnviornmentObject/Contacts.swift
@@ -0,0 +1,11 @@
+//
+//  Points.swift
+//  EnviornmentObject
+//
+//  Created by Arvind Seth on 24/02/23.
+//
+
+import Foundation
+class Contacts: ObservableObject {
+    @Published var mobile: String = String()
+}
